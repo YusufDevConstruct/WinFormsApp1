4168e1ddd556c003eb5f0e073e7c2eccd1b27b26
yusuf <yusuf@YUSUF>
2024-12-11 14:58:23 +0300

    a

--- WinFormsApp1/MalzemeYönetimi.cs
+++ WinFormsApp1/MalzemeYönetimi.cs
@@ -16,5 +16,10 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
